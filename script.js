0d5736794d06baa804a7842fa093da020fc5483e
Sorawit <98515134+Toffy987@users.noreply.github.com>
2025-07-07 21:54:01 +0700

    Update script.js

--- script.js
+++ script.js
@@ -30,20 +30,3 @@ pop.addEventListener('touchstart', () => {
 pop.addEventListener('touchend', () => {
     pop.src = 'pop1.png';
 });
-
-pop.addEventListener('pointerdown', () => {
-    pop.src = 'pop2.png';
-    score++;
-    scoreDisplay.textContent = score;
-    
-    // เล่นเสียง
-    popSound.currentTime = 0; // รีเซ็ตเสียง
-    popSound.play().catch((error) => {
-        console.log('เสียงเล่นไม่ได้จนกว่าจะคลิกครั้งแรก', error);
-    });
-});
-
-pop.addEventListener('pointerup', () => {
-    pop.src = 'pop1.png';
-});
-
